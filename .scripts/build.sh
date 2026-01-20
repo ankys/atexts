764e49b8581081b0e0587713904b915929407937
ankys <ankysp@gmail.com>
2026-01-20 16:17:48 +0000

    add version build

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,6 +1,9 @@
 
-mkdir -p ./output
+VERSION=$(if [ $# -ge 1 ]; then echo "$1"; else echo ""; fi)
+SUFFIX=$(if [ "$VERSION" = "" ]; then echo ""; else echo "-$VERSION"; fi)
 
-typst compile --root . atexts.typ ./output/atexts.pdf --no-pdf-tags
-typst compile --root . cal1t/cal1t.typ ./output/cal1t.pdf --no-pdf-tags
-typst compile --root . cal2t/cal2t.typ ./output/cal2t.pdf --no-pdf-tags
+mkdir -p output
+
+typst compile --root . atexts.typ output/atexts$SUFFIX.pdf --no-pdf-tags
+typst compile --root . cal1t/cal1t.typ output/cal1t$SUFFIX.pdf --no-pdf-tags
+typst compile --root . cal2t/cal2t.typ output/cal2t$SUFFIX.pdf --no-pdf-tags
