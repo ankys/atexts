a3d5ce52bd359b381cb825a05b2eb875266518d9
ankys <ankysp@gmail.com>
2026-03-18 17:07:13 +0000

    add lin1 cal3

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -7,3 +7,5 @@ mkdir -p output
 typst compile --root . atexts.typ output/atexts$SUFFIX.pdf --no-pdf-tags
 typst compile --root . cal1t/cal1t.typ output/cal1t$SUFFIX.pdf --no-pdf-tags
 typst compile --root . cal2t/cal2t.typ output/cal2t$SUFFIX.pdf --no-pdf-tags
+typst compile --root . lin1t/lin1t.typ output/lin1t$SUFFIX.pdf --no-pdf-tags
+typst compile --root . cal3t/cal3t.typ output/cal3t$SUFFIX.pdf --no-pdf-tags
