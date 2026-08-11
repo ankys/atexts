4f25f534486df4a561d63fe00c6c0bdbcec1a5a1
ankys <ankysp@gmail.com>
2026-08-11 00:34:34 +0000

    add astro

---
+++ astro/astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import { typst } from "astro-typst";
+
+// https://astro.build/config
+export default defineConfig({
+	// root: "../",
+	// srcDir: "../src",
+	// base: "",
+	integrations: [
+		starlight({
+			title: "数学テキスト",
+			social: [{ icon: "github", label: "GitHub", href: "https://github.com/ankys/atexts" }],
+			sidebar: [
+			],
+			// disable404Route: true,
+		}),
+		typst({
+			target: (id) => {
+				console.debug(`Detecting ${id}`);
+				if (id.endsWith(".typ") || id.includes('/html/'))
+					return "html";
+				return "svg";
+			},
+			htmlMode: "text",
+		}),
+	],
+});
