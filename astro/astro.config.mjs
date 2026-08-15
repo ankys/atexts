0509a7585219d10fde5bf3f7d32d793325e00316
ankys <ankysp@gmail.com>
2026-08-15 00:21:04 +0000

    add sidebar

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,6 +5,9 @@ import { typst } from "astro-typst";
 
 // https://astro.build/config
 export default defineConfig({
+	// build: {
+	// 	format: "file"
+	// },
 	vite: {
 		resolve: {
 			preserveSymlinks: true
@@ -13,8 +16,16 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: "数学テキスト",
+			locales: { root: { label: "日本語", lang: "ja", }, },
 			social: [{ icon: "github", label: "GitHub", href: "https://github.com/ankys/atexts" }],
 			customCss: ["./src/custom.css"],
+			sidebar: [
+				{ label: "序文", slug: "00_preface", },
+				{ label: "微分積分学１", items: [{ autogenerate: { directory: "cal1t" } }], },
+				{ label: "微分積分学２", items: [{ autogenerate: { directory: "cal2t" } }], },
+				{ label: "微分積分学３", items: [{ autogenerate: { directory: "cal3t" } }], },
+				{ label: "線形代数学１", items: [{ autogenerate: { directory: "lin1t" } }], },
+			],
 		}),
 		typst({
 			target: (id) => {
